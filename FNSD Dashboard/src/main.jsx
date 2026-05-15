0dadc6307ade74acb03b68275919789522df8b2c
Faiq1ktk <145838796+Faiq1ktk@users.noreply.github.com>
2026-05-15 10:30:05 +0500

    Create files and folder of project
    Create files and folder of project

---
+++ FNSD Dashboard/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "antd/dist/reset.css";
+
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/layout.css";
+import "./styles/sidebar.css";
+import "./styles/header.css";
+import "./styles/filterbar.css";
+import "./styles/cards.css";
+import "./styles/tables.css";
+import "./styles/charts.css";
+
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
